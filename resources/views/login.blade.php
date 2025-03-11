89cb511e7e851f2051d2c1fea5707021992b6812
Manori Indika <indikamanori43@gmail.com>
2025-03-11 10:44:10 +0530

    update login page 20250311 01

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,83 +1,102 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable" data-theme="default" data-theme-colors="default">
 <head>
+    <meta charset="utf-8" />
     <title>{{ config('app.name') }} {{ __('Time and Attendance - Secure Login') }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('global.css.php') }}">
-    
-    <script>
-        function bookmarkSite(title, url) {
-            if (window.sidebar) {
-                window.sidebar.addPanel(title, url, "");
-            } else if (window.opera && window.print) {
-                let elem = document.createElement('a');
-                elem.setAttribute('href', url);
-                elem.setAttribute('title', title);
-                elem.setAttribute('rel', 'sidebar');
-                elem.click();
-            } else if (document.all) {
-                window.external.AddFavorite(url, title);
-            }
-        }
-    </script>
-    
-    <style>
-        body {
-            background: url('{{ asset('images/payroll_bg1.png') }}') !important;
-            color: #000;
-            margin: 0;
-            padding: 0;
-            font-family: Verdana, sans-serif;
-            font-size: 11px;
-        }
-        img {
-            border: 0;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Evolve HRM & Payroll System" name="description" />
+    <meta content="Evolve" name="author" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
 </head>
-<body onload="document.login.user_name.focus()">
-    <div id="container">
-        <div id="contentBoxOne">
-            <img src="{{ asset('images/hrm_logo.png') }}" alt="Secure Login" class="imgLock">
+<body>
+    <div class="auth-page-wrapper pt-5">
+        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
+            <div class="bg-overlay"></div>
+            <div class="shape">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1440 120">
+                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+                </svg>
+            </div>
         </div>
-        <div id="rowContentLogin">
-            <form method="post" name="login" action="/authenticate">
-                @csrf
-                <div id="contentBoxLogin">
-                    <div class="textTitle2">
-                        <img src="{{ asset('images/login_top.png') }}" alt="Secure Login" class="imgLock">
-                    </div>
-                    <div id="contentBoxTwo">
-                        @if(session('password_reset'))
-                            <div id="rowWarning" valign="center">
-                                <br>
-                                <b>{{ __('Your password has been changed successfully, you may now login.') }}</b>
-                                <br>&nbsp;
+
+        <div class="auth-page-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mt-sm-5 mb-4 text-white-50">
+                            <div>
+                                <a href="index.html" class="d-inline-block auth-logo">
+                                    <img src="{{ asset('assets/images/logo-light.png') }}" alt="" height="80">
+                                </a>
                             </div>
-                        @endif
-                        <div class="row">
-                            <div class="cellLeft">{{ __('LOGIN') }}</div>
-                            <div class="cellRight"><input type="text" name="user_name" value="{{ old('user_name', $user_name ?? '') }}" size="18"></div>
+                            <p class="mt-3 fs-4 fw-medium">Evolve HRM & Payroll System</p>
                         </div>
-                        <div class="row">
-                            <div class="cellLeft">{{ __('PASSWORD') }}</div>
-                            <div class="cellRight"><input type="password" name="password" size="18"></div>
-                        </div>
-                        <div class="row">
-                            <div class="cellLeft">{{ __('LANGUAGE') }}</div>
-                            <div class="cellRight">
-                                <select name="language">
-                                    @foreach($language_options ?? [] as $key => $value)
-                                        <option value="{{ $key }}" {{ (isset($language) && $language == $key) ? 'selected' : '' }}>{{ $value }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-                        <input type="submit" class="btnLogin" name="action:submit" value="{{ __('Login') }}">
                     </div>
                 </div>
-            </form>
+
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card mt-4 card-bg-fill">
+                            <div class="card-body p-4">
+                                <div class="text-center mt-2">
+                                    <h5 class="text-primary">Welcome Back!</h5>
+                                    <p class="text-muted">Sign in to continue to Evolve HRM & Payroll System.</p>
+                                </div>
+                                <div class="p-2 mt-4">
+                                    <form method="post" name="login" action="/authenticate">
+                                        @csrf
+                                        <div class="mb-3">
+                                            <label for="user_name" class="form-label">LOGIN</label>
+                                            <input type="text" id="user_name" name="user_name" class="form-control" placeholder="Enter your login" value="{{ old('user_name', $user_name ?? '') }}" required autofocus>
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <label for="password" class="form-label">PASSWORD</label>
+                                            <div class="position-relative auth-pass-inputgroup mb-3">
+                                                <input type="password" id="password" name="password" class="form-control pe-5 password-input" placeholder="Enter password" required>
+                                                <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                            </div>
+                                        </div>
+
+                                        <div class="mt-4">
+                                            <button class="btn btn-success w-100" type="submit">Login</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0 text-muted">&copy; <script>document.write(new Date().getFullYear())</script> Evolve </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
-    @include('footer')
+
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <script src="{{ asset('assets/libs/particles.js/particles.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/particles.app.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/password-addon.init.js') }}"></script>
 </body>
 </html>
